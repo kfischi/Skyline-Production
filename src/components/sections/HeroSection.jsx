4eb611f20608edacd9496cded92b986a1b27172f
kfischi <kfir.biz@gmail.com>
2025-11-23 12:18:45 +0200

    Update HeroSection.jsx

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -4,11 +4,18 @@ import styles from './HeroSection.module.css';
 export default function HeroSection() {
   return (
     <section className={styles.hero}>
-      <video autoPlay loop muted playsInline className={styles.heroVideo}>
+      <video 
+        autoPlay 
+        loop 
+        muted 
+        playsInline 
+        className={styles.heroVideo}
+      >
         <source
-          src="https://res.cloudinary.com/dptyfvwyo/video/upload/v1763211981/%D7%95%D7%99%D7%93%D7%90_%D7%A4%D7%AA%D7%99%D7%97%D7%94_wm08bo.mp4"
+          src="https://res.cloudinary.com/dptyfvwyo/video/upload/v1763892387/%D7%95%D7%99%D7%93%D7%90_%D7%A4%D7%AA%D7%99%D7%97%D7%94_wc9vnu.mp4"
           type="video/mp4"
         />
+        הדפדפן שלך לא תומך בתג video
       </video>
       <div className={styles.heroOverlay}></div>
       <div className={styles.heroContent}>
