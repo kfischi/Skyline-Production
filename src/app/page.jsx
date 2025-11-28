d2a0dbffd98fdeb9bc9981f5424b7607b967d492
kfischi <kfir.biz@gmail.com>
2025-11-28 22:08:01 +0200

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,16 @@
 import HeroSection from '@/components/sections/HeroSection';
+import HeroSubSection from '@/components/sections/HeroSubSection';
 import WhyDifferent from '@/components/sections/WhyDifferent';
+import VenuesGallery from '@/components/sections/VenuesGallery';
 import FeaturedProjects from '@/components/sections/FeaturedProjects';
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection />
+      <HeroSubSection />
       <WhyDifferent />
+      <VenuesGallery />
       <FeaturedProjects />
     </div>
   );
